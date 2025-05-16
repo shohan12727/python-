4208154ad271e10abbb1f49e439c7f23ecfe2c09
shohan12727 <aishohan001@gmail.com>
2025-05-16 18:59:24 +0600

    first commit

---
+++ condition.py
@@ -0,0 +1,8 @@
+high_income = True
+good_credit = True
+student = True
+
+if high_income and good_credit and  student :
+  print("eligible")
+else:
+  print("not eligible")  
---
+++ main.py
@@ -0,0 +1,8 @@
+temperature = 37
+if temperature > 30 : 
+  print("It's warm")
+  print("Drink water")
+elif temperature > 20:
+  print("its nice")
+
+print("Done")  
