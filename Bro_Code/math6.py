d8ae1ddcf0627e8438f949cdd9929c0a0d3c433c
shohan12727 <aishohan001@gmail.com>
2025-05-18 00:43:06 +0600

    math basic done

---
+++ Bro_Code/math6.py
@@ -0,0 +1,9 @@
+import math 
+
+a = float(input("Enter a length of a side :"))
+
+b = float(input("Enter a length of a side :"))
+
+result = math.sqrt(pow(a,2) + pow (b,2))
+
+print(f"The value of hypotenuse is {result} cm^2")
