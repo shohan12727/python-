20a1ad09402eb6948e37718fb9c011fc619fbe8b
shohan12727 <aishohan001@gmail.com>
2025-05-17 23:23:41 +0600

    list

---
+++ Tea_Hiteah/basic.py
@@ -0,0 +1,9 @@
+name = "Ashraful Islam Shohan"
+
+print(name.upper())
+print(name.lower())
+
+
+student_name = "Pal Sagar"
+
+print(student_name.replace("Sagar","Pussy"))
---
+++ Tea_Hiteah/list_.py
@@ -0,0 +1,4 @@
+mylist = ["rice","dal","vegetable"]
+print(mylist[0])
+print(mylist[-1])
+
