2514001455e2fab4d4082b83719e520510c9f9ee
shohan12727 <aishohan001@gmail.com>
2025-05-18 01:19:11 +0600

    calculator done

---
+++ Bro_Code/condition50.py
@@ -0,0 +1,12 @@
+age = int(input("Enter your age : "))
+
+if age >= 18 :
+  print(f"As you are {age} , Now you can vote for your country")
+
+elif age <= 0 :
+  print("Invalid value !")
+
+else:
+  print(f"As you are {age}, You can't vote for your country")  
+
+  
