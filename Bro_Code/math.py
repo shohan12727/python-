c548d826baefa7bac3a9ba59ca65cb5b54e9645a
shohan12727 <aishohan001@gmail.com>
2025-05-17 23:54:14 +0600

    math more

--- Bro_Code/math.py
+++ Bro_Code/math.py
@@ -1,7 +1,7 @@
-# friends = ["shohan","rakib","raju"]
+friends = ["shohan","rakib","raju"]
 
-# for friend in friends :
-#   print(friend)
+for friend in friends :
+  print(friend)
 
 print(abs(-8))
 
@@ -11,3 +11,8 @@ print(abs(8+12j))
 
 print(abs(5555))
 
+print (round(5.985))
+print (round(5.49))
+
+print(pow(5,2))
+print(pow(10,5))
