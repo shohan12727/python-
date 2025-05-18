89b1d6d0b423748e8e351cbe7084d27aa33fc494
shohan12727 <aishohan001@gmail.com>
2025-05-18 20:41:10 +0600

    update

--- Bro_Code/cond.py
+++ Bro_Code/cond.py
@@ -3,3 +3,10 @@ num = - 85
 print ("Positive" if num > 0 else "negative " )
 
 print ("even" if num %2 == 0 else "odd") 
+
+a = 5 
+b = 10 
+
+print ("a is bigger" if a>b else "b is bigger number ")
+
+
