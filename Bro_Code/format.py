4deca7f6464dfb3f812842bfabe345a713c7f528
shohan12727 <aishohan001@gmail.com>
2025-05-19 00:11:36 +0600

    format spacifier done

---
+++ Bro_Code/format.py
@@ -0,0 +1,7 @@
+price1 = 3.5644
+price2 = -6565.654
+price3 = 0.54
+
+print(f"the price of first product is ${price1:.2f}")
+print(f"the price of second product is ${price2:.2f}")
+print(f"the price of third product is ${price3:.2f}")
---
+++ Bro_Code/reverse.py
@@ -0,0 +1,3 @@
+creditNumber = "24-5454-2454-454"
+creditNumber= creditNumber[::-1]
+print(creditNumber)
